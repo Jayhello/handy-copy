d5ee17a01edbee79e451a40cab7f84a5a9da872c
Jayhello <haluoluo@qq.com>
2018-12-03 10:42:38 +0800

    1. add select example

---
+++ handy/handy.h
@@ -0,0 +1,16 @@
+//
+// Created by root on 11/22/18.
+//
+
+#ifndef ECHO_HANDY_H
+#define ECHO_HANDY_H
+#include "conf.h"
+#include "daemon.h"
+#include "file.h"
+#include "http.h"
+#include "logging.h"
+#include "slice.h"
+#include "threads.h"
+#include "udp.h"
+#include "util.h"
+#endif //ECHO_HANDY_H
